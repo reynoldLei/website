69b447fd98c674f8ea1f8620e33ccc5733c7d3f3
reynoldLei <longcongyuzhi@gmail.com>
2014-10-08 20:57:12 -0700

    solve image output problem

---
+++ handlebarHelpers.js
@@ -0,0 +1,19 @@
+var Handlebars = require('express-hbs');
+// var _ = require('lodash');
+var registerHelper = function (){
+    Handlebars.registerHelper('imageUrl', function imageUrl(html) {
+        var res = html.match(/\<img src=\"(.*)\" alt=\"event-cover\" \/\>/)
+        return res && res[1];
+    });
+    Handlebars.registerHelper('timeTag', function timeTag(html){
+    	var arr= html.match(/\<time src=\"(.*)\"\/\>/);
+    	return arr && arr[1];
+    });
+    Handlebars.registerHelper('addrTag', function addrTag(html){
+        console.log(html);
+    	var arr= html.match(/\<addr src=\"(.*)\"\/\>/);
+    	return arr && arr[1];
+    });
+};
+
+module.exports = registerHelper;
